af48cb6dc1bcb16b1a3e2a82581ee86db76c4118
Ali Elsokary <ali.elsokary8@gmail.com>
2021-10-20 18:39:35 +0200

    Add EventsServiceMock

--- EventAppTests/EventsServiceMock.swift
+++ EventAppTests/EventsServiceMock.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import RxSwift
+@testable import EventApp
+
+class EventsServiceMock: EventsService {
+	
+	var eventTypesReturnValue: Observable<EventTypes> = .empty()
+
+	func getEventTypes() -> Observable<EventTypes> {
+		return eventTypesReturnValue
+	}
+	
+	var eventsReturnValue: Observable<Events> = .empty()
+	var eventTypeArgument: String!
+	var eventPageArgument: Int!
+
+	func getEvents(type: String, page: Int) -> Observable<Events> {
+		eventTypeArgument = type
+		eventPageArgument = page
+		return eventsReturnValue
+	}
+}
